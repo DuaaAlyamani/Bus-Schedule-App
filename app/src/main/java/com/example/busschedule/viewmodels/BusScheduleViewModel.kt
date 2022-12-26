3d8ac505032afad815f43436bcfc2779bab6a801
alyam <dalyamanis4@gmail.com>
2022-12-26 13:38:55 +0300

    add data by using Room and Flow

---
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.busschedule.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.busschedule.database.database.schedule.Schedule
+import com.example.busschedule.database.database.schedule.ScheduleDao
+import kotlinx.coroutines.flow.Flow
+
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
+
+    fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
+}
